29e620a2d8650baf47d2b66c63a015b9c6b607a3
Joabsys <145292072+Joabsys@users.noreply.github.com>
2026-03-24 17:14:30 -0300

    ex

---
+++ ex03/valida_numeros.py
@@ -0,0 +1,10 @@
+# o int faz um cast no input,pois o input acha que é uma string
+#então usamos o int para converter o input no tipo desejado.
+
+numero = int(input("Digite um numero: "))
+if numero > 0:
+    print("o numero é positivo.")
+elif numero < 0:
+    print(" o numero é negativo.")
+else
+    print(" o numero é zero.")
